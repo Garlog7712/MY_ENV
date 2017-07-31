9e2f62104ced6014c6435604e232f1296b239407
Dana Maxfield <dmaxfield@blackducksoftware.com>
2017-07-31 13:41:11 -0400

    Adding my files to correct repo

---
+++ brew_installs.sh
@@ -0,0 +1,29 @@
+brew update
+##brew install findutils --default-names
+##brew install gnu-sed --default-names
+##brew install gnu-tar --default-names
+##brew install gnu-which --default-names
+##brew install gnutls --default-names
+##brew install grep --default-names
+##brew install coreutils
+##brew install binutils
+##brew install diffutils
+##brew install gzip
+##brew install watch
+##brew install tmux 
+##brew install wget
+##brew install nmap
+##brew install gpg
+##brew install htop
+##brew cask install java
+##brew install maven
+##brew install maven-completion
+##brew cask install netbeans
+##brew install groovy
+##brew install grails
+##brew install gradle
+##brew install git-when-merged
+##brew install shellCheck
+##brew install unix2dos
+##brew install dos2unix
+brew install jq
